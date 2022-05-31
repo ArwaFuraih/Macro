6f519a316dd803f06d672d8ebf9bde469b880894
Arwa Alfuraih <Arwaafuraih@gmail.com>
2022-05-31 18:27:39 +0300

    Initial Commit

---
+++ Macro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Macro
+//
+//  Created by Arwa Alfuraih on 31/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Macro/MacroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroApp.swift
+//  Macro
+//
+//  Created by Arwa Alfuraih on 31/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MacroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
